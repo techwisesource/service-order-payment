45993bf4c1f1387adaadbdb1876d0995ad63f2e8
aryandh.a.w <aryandh.a.w@gmail.com>
2024-11-13 14:41:41 +0700

    create orderController and function store

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+
+    public function store(Request $request)
+    {
+        $user = $request->input('user');
+        $course = $request->input('course');
+
+        $order = Order::create([
+            'user_id' => $user['id'],
+            'course_id' => $course['id']
+        ]);
+
+        return response()->json($order);
+    }
+
+    private function getlMidtransSnapUr()
+    {
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        \Midtrans\Config::$isProduction = (bool) env('MIDTRANS_PRODUCTION');
+        \Midtrans\Config::$is3ds = (bool) env('MIDTRANS_3DS');
+
+        $snapUrl = \Midtrans\Snap::createTransaction($params)->redirect_url;
+        return $snapUrl;
+    }
+}
