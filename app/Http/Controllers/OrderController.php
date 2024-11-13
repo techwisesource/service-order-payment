907583bf9cdc38ff1864e757a0815ff19cbd7314
aryandh.a.w <aryandh.a.w@gmail.com>
2024-11-13 17:06:13 +0700

    endpoint get orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,6 +9,21 @@ use Illuminate\Support\Str;
 class OrderController extends Controller
 {
 
+    public function index(Request $request)
+    {
+        $userId = $request->input('user_id');
+        $orders = Order::query();
+
+        $orders->when($userId, function($query) use ($userId) {
+            return $query->where("user_id", "=", $userId);
+        });
+
+        return response()->json([
+            "status" => "success",
+            "data" => $orders->get()
+        ]);
+    }
+
     public function store(Request $request)
     {
         $user = $request->input('user');
